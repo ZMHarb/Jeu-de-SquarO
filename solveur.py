af5517a67308b62b74ae649697cbbffc46ab7965
Ziad Mafadi <105078630+ZiadMafadi@users.noreply.github.com>
2022-05-12 13:49:16 +0200

    Update solveur.py

--- solveur.py
+++ solveur.py
@@ -13,17 +13,23 @@ from z3 import *
 
 class SAT_Solveur:
     
-    '''
-    Une fonction qui permette de bloquer un model de SAT solveur, pour looper sur tous les models possibles
-    '''
     def block_model(self, model):
+        '''
+        Une fonction qui permette de bloquer un model de SAT solveur, pour looper sur tous les models possibles
+        :param model: un model donné par le Z3 solveur
+        '''
         m = model
         self.s.add(Or([ f() != m[f] for f in m.decls() if f.arity() == 0])) 
 
-    #   return: 1 -> le tableau est résoluble (sat problem)
-    #   return: 0 -> le tableau n'est pas résoluble (unsat problem)
     def solve_SAT(self, fichier):
+        '''
+        solve_SAT donne le fichier DIMACS au Z3 solveur afin de verifier si le fichier est solvable
+        
+        :param fichier: Un fichier DIMACS
 
+        :return: 1 -> Le tableau est solvable
+        :return: 0 -> Le tableau n'est pas solvable
+        '''
         #On initialise le solveur
         self.s = Solver()
 
@@ -62,12 +68,18 @@ class SAT_Solveur:
             return 1
     
     
-    '''
-    Fonction utilisée pour ecrire les solutions dans un fichier au format SAT
-    coin coché      -> 1 
-    coin non coché  -> 0 
-    '''
     def ecrire_fichier_SAT(self, dimension, cases, fichier):
+
+        '''
+        Fonction utilisée pour ecrire les solutions dans un fichier au format SAT
+        coin coché      -> 1 
+        coin non coché  -> 0 
+        
+        :param dimension: La dimension de la grille.
+        :param cases: Une double Liste qui contient les coordonnés des coins cochés.
+        :param fichier: Le fichier sortie
+        :return : None
+        '''
 
         n = dimension + 1
         with open(fichier, "w") as f:
@@ -80,52 +92,114 @@ class SAT_Solveur:
                 f.write('\n')
 
 
-    '''
-    Une fonction qui donne les coins du grille avec differents nomenclatures selon la dimension, 
-    utile pour générer fichier dimacs.
-    '''
     def cases_dimacs_vide(self, dimension):
-            
-        cases = [[1, 2], [2, 5]]
-        max = 5
-        for _ in range(2, dimension):
 
-            nbr = cases[len(cases)-1][1]
-            liste = [nbr, nbr + 2]
-            cases.append(liste)
-    
+        '''
+        Une fonction qui donne les coins de la grille avec differents nomenclatures selon la dimension, 
+        utile pour générer fichier DIMACS.
 
-        cases.append([3, 4])
-        cases.append([4, 6])
-        for _ in range(2, dimension):
+        :param dimension: La dimension de la grille
 
-            nbr = cases[-1][1]
-    
-            liste = [nbr, nbr + 2]
-            cases.append(liste)                
-            max = nbr + 2
+        :return cases: Les cases de la grille avec differents nomenclatures pour chaque coin 
+        '''            
 
-        for _ in range(dimension - 1):
+        if dimension < 2:
+            cases = [[1, 2], [3, 4]]
+        
+        else:
+            #On ajoute au cases, les 2 premiers indices du premier ligne.
+            cases = [[1, 2], [2, 5]]
 
-            liste = [max + 1, max + 2]
-            cases.append(liste)
-            max += 1
-            for _ in range(dimension-1):
+            #Le max des valeurs entrées au 'cases'
+            max = 5
+
+            #On ajoute le reste des indices dans le premier ligne
+            for _ in range(2, dimension):
+
+                nbr = cases[len(cases)-1][1]
+                liste = [nbr, nbr + 2]
+                cases.append(liste)
+        
+            #On ajoute au cases, les 2 premiers indices du deuxieme ligne
+            cases.append([3, 4])
+            cases.append([4, 6])
+
+            #On ajoute le reste des indices du deuxieme ligne
+            for _ in range(2, dimension):
+
+                nbr = cases[-1][1]
+        
+                liste = [nbr, nbr + 2]
+                cases.append(liste)                
+                max = nbr + 2
+
+            #On ajoute le reste des indices de toute la grille
+            for _ in range(dimension - 1):
 
                 liste = [max + 1, max + 2]
                 cases.append(liste)
                 max += 1
-            max+=1
-            
+                for _ in range(dimension-1):
+
+                    liste = [max + 1, max + 2]
+                    cases.append(liste)
+                    max += 1
+                max+=1
+                
         return cases
     
+    def recup_tableau_dimacs(self, cases_dimacs_vide, dimension):
+    
+        '''
+        Une fonction qui donne le grille correspondant au DIMACS
+        cad differents nomenclature pour chaque coin du case
 
-    '''
-    Une fonction qui fait echanger les variables d'une clause avec ce qui correspond dans le grille
-    old_string      -> la clause avec (1, 2, 3, 4) comme variables
-    variables_case  -> les variables qui doivent remplacer (1, 2, 3, 4) dans la clause de Dimacs
-    '''
+        :param cases_dimacs_vide: Les cases de la grille avec differents nomenclatures
+        :param dimension : La dimension du tableau
+
+        :return tableau: Une double Liste correspondante à la grille avec differents nomenclatures pour tous ses coins 
+        '''
+
+        tableau = []
+        n = dimension        
+        
+        i = 0
+        iter = 0
+        
+        #Un tableau de dimension n, contient n*(n+1) cases
+        while i < ( n * (n+1)):
+            
+            liste = []
+
+            #Le nbr de cases per ligne
+            nbr = n + iter*n
+            
+            #Ajouté seulement les cases correspondante au ligne courant
+            for l in cases_dimacs_vide[i:nbr]:
+                liste.extend(l)
+            
+            #Si une liste d'une cases est ajouté deux fois, on doit la supprimer
+            for num in liste:
+                if liste.count(num) > 1 : 
+                    liste.remove(num)
+            
+            tableau.append(liste)
+            i += n
+            iter += 1
+
+        return tableau
+
+
     def replace_variables(self, old_string, variables_case):
+    
+        '''
+        Une fonction qui fait echanger les variables d'une clause avec ce qui correspond dans le grille
+
+        :param old_string: La clause avec (1, 2, 3, 4) comme variables
+        :param variables_case: Les variables qui doivent remplacer (1, 2, 3, 4) dans la clause de Dimacs
+        
+        :return: La clause apres changement de variables
+        '''
 
         s = old_string.split()
         new_string = ""
@@ -150,40 +224,22 @@ class SAT_Solveur:
 
         return new_string
 
-    '''
-    Une fonction qui donne le grille correspondant au DIMACS
-    cad differents nomenclature pour chaque coin du case
-    '''
-    def recup_tableau_dimacs(self, cases_dimacs_vide, dimension):
-        
-        tableau = []
-        n = dimension        
-        i = 0
-        iter = 0
-        while i < ( n * (n+1)):
-            
-            liste = []
-            nbr = n + iter*n
-            
-            for l in cases_dimacs_vide[i:nbr]:
-                liste.extend(l)
-            
-            for num in liste:
-                if liste.count(num) > 1 : 
-                    liste.remove(num)
-            
-            tableau.append(liste)
-            i += n
-            iter += 1
-
-        return tableau
-
-    '''
-    Fonction utilisée pour ecrire DIMACS d'un probleme dans un fichier
-    '''    
     def ecrire_DIMACS(self, dimension, tableau, fichier):
 
+        '''
+        Fonction utilisée pour ecrire DIMACS d'un probleme dans un fichier
+
+        :param dimension: La dimension de la grille
+        :param tableau: Le tableau qui contient la grille
+        :param fichier: Le fichier sortie
+
+        :return : None
+
+        '''    
+
         n = dimension
+
+        #Un dicto qui contient les infos utiles lors du l'ecriture de fichier DIMACS
         dicto = {
             '0':{'nbr_clauses': 4, 'nbr_variables': 4},
             '1':{'nbr_clauses': 7, 'nbr_variables': 4},
@@ -193,11 +249,12 @@ class SAT_Solveur:
         }
 
         clauses = []
+
+        #Pour ecrire le nbr_clauses totale dans le fichier DIMACS
         nbr_clauses_total = 0
 
-        if dimension > 1:
-            cases_tab_vide = self.cases_dimacs_vide(dimension)
-
+        cases_tab_vide = self.cases_dimacs_vide(dimension)
+        
         #utilisé lors de l'affichage du solution dans le fichier squaro.py
         self.dimacs_tableau = self.recup_tableau_dimacs(cases_tab_vide, dimension)
 
@@ -236,6 +293,7 @@ class SAT_Solveur:
 
         f = open(fichier, "w")
 
+        #On ecrit la premiere ligne essentielle pour le fichier DIMACS
         f.write("p cnf {} {}\n".format(nbr_variables_total, nbr_clauses_total))
 
         for i in range(len(clauses)):
